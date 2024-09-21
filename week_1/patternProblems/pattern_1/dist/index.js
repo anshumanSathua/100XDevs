dfca5834dea750f55d3853048d36935dc838d5d8
Anshuman Sathua <co.anshu@gmail.com>
2024-09-21 22:21:00 +0530

    pattern_1

--- week_1/patternProblems/pattern_1/dist/index.js
+++ week_1/patternProblems/pattern_1/dist/index.js
@@ -1,1 +1,20 @@
 "use strict";
+/**
+ *Square Star Pattern in Javascript
+ *           *****
+ *           *****
+ *           *****
+ *           *****
+ *           *****
+ */
+const squarePattern = (n) => {
+    let str = "";
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            str += "*";
+        }
+        str += "\n";
+    }
+    return str;
+};
+console.log(squarePattern(5));
--- week_1/patternProblems/pattern_1/src/index.ts
+++ week_1/patternProblems/pattern_1/src/index.ts
@@ -1,3 +1,21 @@
 /**
- *
+ *Square Star Pattern in Javascript
+ *           *****
+ *           *****
+ *           *****
+ *           *****
+ *           *****
  */
+
+const squarePattern = (n: number): string => {
+  let str: string = "";
+  for (let i: number = 0; i < n; i++) {
+    for (let j: number = 0; j < n; j++) {
+      str += "*";
+    }
+    str += "\n";
+  }
+  return str;
+};
+
+console.log(squarePattern(5));
